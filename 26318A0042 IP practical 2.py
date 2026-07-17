91f4d358a7be7850651a97be03b6df9743b30a70
Saurabh-Gupta67 <rajg27531@gmail.com>
2026-07-17 10:36:29 +0530

    Add files via upload

---
+++ 26318A0042 IP practical 2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 10:28:02 2026
+
+@author: Student
+"""
+#Q1
+
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+
+print("Simple Print")
+print(name,age)
+
+print("\nformatted String:")
+print(f"My name is {name} and i am {age} years old.")
+
+#Q2
+
+
